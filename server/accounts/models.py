833abc93b84d815e946a647de0b5f086aa9c2c82
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2026-01-09 22:23:01 +0530

    chore: ran migrations on custom user model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -26,6 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    google_sub = models.CharField(max_length=255, blank=True, null=True)
 
     onboarding_completed = models.BooleanField(default=False)
 
